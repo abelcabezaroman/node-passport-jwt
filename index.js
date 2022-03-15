7b0d1892f49e0ca31480c3d8782a003f6123640a
Abel Cabeza Román <contacto@abelcabezaroman.com>
2022-03-16 00:26:21 +0100

    init

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+require('./utils/db')
+const passport = require('passport');
+require('./authentication/passport'); // Requerimos nuestro archivo de configuración
+
+// En nuestro archivo index.js
+const userRoutes = require('./routes/user.routes');
+const authRoutes = require('./routes/auth.routes');
+
+const PORT = 3000;
+const server = express();
+server.set("secretKey", "nodeRestApi");
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+
+// Añadimos el nuevo middleware al servidor
+server.use(passport.initialize())
+
+
+server.use('/users', userRoutes);
+server.use('/', authRoutes);
+
+server.use('*', (req, res, next) => {
+    const error = new Error('Route not found');
+    error.status = 404;
+    next(error);
+});
+
+server.use((error, req, res, next) => {
+    return res.status(error.status || 500).json(error.message || 'Unexpected error');
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running in http://localhost:${PORT}`);
+});
---
+++ utils/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+// URL local de nuestra base de datos en mongoose y su nombre upgrade_class_3
+const DB_URL = 'mongodb://localhost:27017/register-login';
+
+// Función que conecta nuestro servidor a la base de datos de MongoDB mediante mongoose
+mongoose.connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
